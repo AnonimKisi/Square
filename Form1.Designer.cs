ffecbc147327f60bab6ebe8f00f8affc65980986
AnonimKisi <67228486+AnonimKisi@users.noreply.github.com>
2021-03-08 17:48:12 +0300

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -759,7 +759,7 @@ namespace Snake
             this.Controls.Add(this.label29);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Form1";
-            this.Text = "Made by AnonimKisi";
+            this.Text = "Score: 0";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
